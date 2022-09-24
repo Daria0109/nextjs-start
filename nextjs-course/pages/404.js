44840d855d1249073ad26864947970caaad00ff9
Darya Shnipova <darya.shnipova@degree53.com>
2022-09-24 23:01:39 +0400

    Created routs structure files

---
+++ nextjs-course/pages/404.js
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/router';
+
+export default function NotFoundPage () {
+    return (
+        <div>
+            <h1>
+                404: Page is not found!
+            </h1>
+        </div>
+    )
+}
---
+++ nextjs-course/pages/events/[eventId].js
@@ -0,0 +1,7 @@
+export default function EventDetailPage () {
+    return (
+        <div>
+            <h1>The specific event</h1>
+        </div>
+    )
+}
